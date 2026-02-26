3019b7f9e4a1fa52bb5e98e32d421527fac62695
bruceryu77 <bruceryu77@gmail.com>
2026-02-26 14:57:08 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'invoice-barcode-pwa-v1';
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          // 네트워크/캐시 모두 실패 시에는 그냥 실패 반환
+          return response;
+        })
+      );
+    })
+  );
+});
+
